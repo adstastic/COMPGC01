11e724fc1736fb7be4842cb9b8dbfa5ac921ae67
Ads <ads.mukherjee@gmail.com>
2015-10-22 15:35:13 +0100

    Finished tutorial 2

--- src/Tutorial_2/ArrayListAndRandomNumbers.java
+++ src/Tutorial_2/ArrayListAndRandomNumbers.java
@@ -1,6 +1,5 @@
 package Tutorial_2;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -8,49 +7,45 @@ public class ArrayListAndRandomNumbers {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		List<Integer> randArrList= new ArrayList<Integer>();
-		List<Integer> oddRandArrList= new ArrayList<Integer>();
+		ArrayList<Integer> aList= new ArrayList<Integer>();
+		ArrayList<Integer> oddaList= new ArrayList<Integer>();
 		int sum = 0;
 		
 		/* populate ArrayList with random numbers */
 		for (int i=0; i<100; i++) {
 			int currentRand = random.nextInt(100);
-			randArrList.add(random.nextInt(100));
+			aList.add(random.nextInt(100));
 			if (ProgramFlowAndBooleans.isEven_noLocal(currentRand)) {
 				sum += currentRand;
 			/* separate out odd numbers as they are added into randArrList for use later */
 			} else {
-				oddRandArrList.add(currentRand);
+				oddaList.add(currentRand);
 			}
 		}
-		System.out.println("Random ArrayList: "+randArrList.toString());
+		System.out.println("Random ArrayList: "+aList.toString());
 		System.out.println("Sum: "+sum);
 		
 		/* Remove odd elements from randArrList */
-//		randArrList.removeAll(oddRandArrList); // Not working for some reason
+//		aList.removeAll(oddaList); // Not working for some reason
 		
-		/* throws ConcurrentModificationException */
-//		for (Integer randomNumber : randArrList) {
-//			if (oddRandArrList.contains(randomNumber)) {
-//				randArrList.remove(randomNumber);
-//			}
-//		}
-		
-		for (int i=0; i<randArrList.size(); i++) {
-			if (ProgramFlowAndBooleans.isEven_noLocal(randomNumber)) {
-				randArrList.remove(randomNumber);
+		for (int i=0; i<aList.size(); i++) {
+			int randomNumber = aList.get(i);
+			if (!ProgramFlowAndBooleans.isEven_noLocal(randomNumber)) {
+				aList.remove(i);
+				i--;
 			}
 		}
 		
-		System.out.println("Random Arraylist of even: "+randArrList);
-		System.out.println("Random Arraylist of odd: "+oddRandArrList);
-		randArrList.addAll(oddRandArrList);
-		System.out.println("Random ArrayList with even separated from odd: "+randArrList.toString());
+		System.out.println("Random Arraylist of even: "+aList);
+		System.out.println("Random Arraylist of odd: "+oddaList);
+		aList.addAll(oddaList); // not adding elements back in at end
+		
+		System.out.println("Random ArrayList with even separated from odd: "+aList.toString());
 		
 		/* convert to int Array */
-		int [] randArr = new int[randArrList.size()];
-		for (int i=0; i<randArrList.size(); i++) {
-			randArr[i] = randArrList.get(i);
+		int [] randArr = new int[aList.size()];
+		for (int i=0; i<aList.size(); i++) {
+			randArr[i] = aList.get(i);
 		}
 		
 		System.out.println("Random Array: "+Arrays.toString(randArr));
