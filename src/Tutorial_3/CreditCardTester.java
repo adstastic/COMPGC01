d44dd1f4f312718fec84d8f0369953883a65d936
Ads <ads.mukherjee@gmail.com>
2015-10-26 16:12:26 +0000

    Finished tutorial 3

---
+++ src/Tutorial_3/CreditCardTester.java
@@ -0,0 +1,14 @@
+package Tutorial_3;
+
+public class CreditCardTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CreditCard c1 = new CreditCard(10, 2014, "Bob", "Jones", "1234567890123456");
+		CreditCard c2 = new CreditCard(10, 2015, "Bob", "Jones", "1234567890123456");
+		
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+	}
+
+}
