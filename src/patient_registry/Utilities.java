ee63023f193450665f6cfccee8748cacab85722f
Ads <ads.mukherjee@gmail.com>
2015-12-01 18:44:37 +0000

    JSON library added, login UI completed

--- src/patient_registry/Utilities.java
+++ src/patient_registry/Utilities.java
@@ -1,5 +1,9 @@
 package patient_registry;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,55 +11,67 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class Utilities {
-	private static HashMap<String, String> user_map = new HashMap<String, String>();
+	private static HashMap<String, String> userMap = new HashMap<String, String>();
 	
-	public static boolean authenticate(String username, char[] password) {
+	public static boolean authenticate(String username, String password) {
 		boolean login = false;
-		String password_str = Arrays.toString(password);
-		Iterator<Entry<String, String>> users = user_map.entrySet().iterator();
+		Iterator<Entry<String, String>> users = userMap.entrySet().iterator();
 		while(users.hasNext()) {
 			Entry<String, String> entry = (Entry<String, String>) users.next(); 
-			if (entry.getKey().equals(username) && entry.getValue().equals(password_str)) {
+			if (entry.getKey().equals(username) && entry.getValue().equals(password)) {
 				login = true;
 			} 
 		} 
 		return login;
 	}
 	
-	public void add_user(String username, String password) {
-		user_map.put(username, password);
-	}
-	
-	public void read(String filepath) {
+	public static boolean addUser(String username, String password) {
+		userMap.put(username, password);
 		try {
-			File user_map_obj = new File("./user_map.ser");
-			if (user_map_obj.exists()) { 
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream("user_map.ser"));
-				user_map = (HashMap<String, String>) ois.readObject();
-				ois.close();
-			}
+			XMLencode(userMap, "users.xml");
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
+		}g 
+	}
+	
+	public void readObject(String filepath) throws Exception {
+		File userMapFile = new File("./user_map.ser");
+		if (userMapFile.exists()) { 
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("user_map.ser"));
+			userMap = (HashMap<String, String>) ois.readObject();
+			ois.close();
 		}
 	}
 	
-	public void write() {
-		try {
-			File user_map_obj = new File("./user_map.ser");
-			if (user_map_obj.exists()) { 
-				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("user_map.ser"));
-				oos.writeObject(user_map);
-				oos.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static void writeObject() throws Exception {
+		File userMapObj = new File("./user_map.ser");
+		if (userMapObj.exists()) { 
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("user_map.ser"));
+			oos.writeObject(userMap);
+			oos.close();
 		}
 	}
+	
+	public static void XMLencode(HashMap<String, String> userMap, String filename) throws Exception {
+		XMLEncoder e = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
+		e.writeObject(userMap);
+		e.close();
+	}
+	
+	public static HashMap<String, String> XMLdecode(String filename) throws Exception {
+		XMLDecoder d = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
+//			if (d.readObject().getClass() == HashMap.class) {				
+			HashMap<String, String> userMap = (HashMap<String, String>) d.readObject();
+//			}
+		d.close();
+		return userMap;
+	}
+	
 }
